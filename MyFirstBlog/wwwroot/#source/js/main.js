48a2defb1034c7d1baff9a0d9180513ce0f44be6
Maxim <yourstenex@gmail.com>
2020-07-23 16:01:29 +0300

    Update index page
    Add slider and template of article

--- MyFirstBlog/wwwroot/#source/js/main.js
+++ MyFirstBlog/wwwroot/#source/js/main.js
@@ -2,7 +2,9 @@
 
 
 let menuButton = document.querySelector('.main-nav__button');
-let menuList = document.querySelector('.main-nav__list')
+let menuList = document.querySelector('.main-nav__list');
+let sliderItems = document.querySelectorAll('.articles-slider__item');
+let currentSlide = 0;
 
 menuButton.addEventListener('click', function (evt) {
    if (menuButton.classList.contains('main-nav__button--closed')) {
@@ -16,4 +18,33 @@ menuButton.addEventListener('click', function (evt) {
       menuList.classList.remove('main-nav__list--opened');
       menuList.classList.add('main-nav__list--closed');
    }
-})
+});
+
+function slider() {
+   for (let i = 0; i < sliderItems.length; i++) {
+      sliderItems[i].classList.remove('articles-slider__item--active');
+   }
+   sliderItems[currentSlide].classList.add('articles-slider__item--active');
+}
+
+
+
+document.querySelector('.articles-slider__btn--prev').onclick = function () {
+   if (currentSlide - 1 < 0) {
+      currentSlide = sliderItems.length - 1;
+   }
+   else {
+      currentSlide--;
+   }
+   slider();
+}
+
+document.querySelector('.articles-slider__btn--next').onclick = function () {
+   if (currentSlide + 1 == sliderItems.length) {
+      currentSlide = 0
+   }
+   else {
+      currentSlide++;
+   }
+   slider();
+}
